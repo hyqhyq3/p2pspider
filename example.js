63aab0e8cd6aa29b75015a8a9f639594c48ba80e
hyq <i@hyq.me>
2016-02-29 05:26:33 -0500

    add sqlite3

--- example.js
+++ example.js
@@ -1,9 +1,17 @@
 "use strict"
 
+var sqlite3 = require('sqlite3').verbose();
+var util = require('util');
+var db = new sqlite3.Database('./data.db');
+db.serialize(()=>{
+	db.run("CREATE TABLE if not exists link (infohash text PRIMARY KEY, name text, link text)");
+});
+
 var DHTSpider = require('./p2pspider/dhtspider');
 var BTClient = require('./p2pspider/btclient');
 
 var btclient = new BTClient({ timeout: 1000 * 10 });
+var stmt = db.prepare("insert OR IGNORE into link (infohash, name, link) values(?,?,?)");
 btclient.on('complete', (metadata, infohash, rinfo) => {
 
     // metadata.info 含有资源名字, 资源大小, 资源文件列表等信息.
@@ -13,7 +21,9 @@ btclient.on('complete', (metadata, infohash, rinfo) => {
         console.log('\n');
         console.log('name: %s', name.toString());
         console.log('from: %s:%s', rinfo.address, rinfo.port );
-        console.log('link: magnet:?xt=urn:btih:%s', infohash.toString('hex'));
+        var link = util.format('magnet:?xt=urn:btih:%s', infohash.toString('hex'));
+	console.log('link: ' + link);
+	stmt.run(infohash.toString('hex'), name.toString(), link)
     }
 });
 
